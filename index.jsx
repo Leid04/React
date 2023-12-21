d472f558b6cd685bd294a5629ec87227a964d2be
Denys Revutskyi <denys.msi04@gmail.com>
2023-12-21 20:02:22 +0100

    scripts empaquetado linting yarn npm nodejs

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {createRoot} from "react";
+
+const root = document.querySelector("#root");
+
+const app = (
+    <div>My wishlist from React</div>
+);
+
+const rootElement = createRoot(root);
+rootElement.render(app);
