0bb149d3a7a959129c7cd2bb78621b41a4658c45
Denys Revutskyi <denys.msi04@gmail.com>
2024-01-01 15:57:50 +0100

    tareas vd

--- tareas/src/Componentes/ListaDeTareas.jsx
+++ tareas/src/Componentes/ListaDeTareas.jsx
@@ -5,10 +5,28 @@ import Tarea from "./Tarea";
 
 export default function ListaDeTareas() {
   const [tareas, setTareas] = useState([]);
+
   const agregarTarea = (tarea) => {
-    console.log(tarea);
-    console.log('Tarea agreagada');
-  }
+    if(tarea.texto.trim()){//Si no esta en blanco.
+      tarea.texto = tarea.texto.trim();//Quitar espacios.
+      const tareasActualizadas = [tarea, ...tareas];//La tarea nueva al principio y las anteriores.
+      setTareas(tareasActualizadas);//Actualizamos el array de tareas.
+    } 
+  };
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  };
+
+  const completarTarea = (id) => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if(tarea.id === id){
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  };
 
   return (
     <>
@@ -17,8 +35,12 @@ export default function ListaDeTareas() {
         {
           tareas.map((tarea) =>
             <Tarea
+              key={tarea.id}//Para que sepa el orden al actualizarlos.
+              id={tarea.id}//Para poder luego acceder porque a key no se puede acceder.
               texto={tarea.texto}
               completada={tarea.completada}
+              completarTarea={completarTarea}
+              eliminarTarea={eliminarTarea}
             />
           )
         }
